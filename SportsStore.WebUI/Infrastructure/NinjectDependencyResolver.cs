86f7f9bb5bf252c443649d7feed9c4fe2df1d6b6
Fábio Moura de Oliveira <ccmaismais@yahoo.com>
2016-10-01 19:58:13 -0300

    Leitura do capítulo 12 terminada, tudo funcionando corretamente.

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -8,6 +8,9 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.Domain.Concrete;
 using System.Configuration;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
+
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -42,15 +45,7 @@ namespace SportsStore.WebUI.Infrastructure
             kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().
                 WithConstructorArgument("settings", emailSettings);
 
-            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            //mock.Setup(m => m.Products).Returns(new List<Product>
-            //{
-            //    new Product {Name = "Football", Price = 25 },
-            //    new Product {Name = "Surf board", Price = 179 },
-            //    new Product {Name = "Running shoes", Price = 95 }
-            //});
-            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
-
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
         }
     }
 }
